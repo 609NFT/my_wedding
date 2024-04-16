e0303f3e6d483bf15149f42a82c0ab59d59e6f54
609NFT <boisjolibrian@gmail.com>
2024-04-16 17:23:52 -0400

    build fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="#"
+          href="https://brianboisjoli.com/"
           rel="noopener noreferrer"
         >
           Enter Site
