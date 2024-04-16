e1704b78f810303c3b17013609059c5f22e3fbf3
609NFT <boisjolibrian@gmail.com>
2024-04-16 18:44:48 -0400

    updates

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../h_b.png';  // Adjust the path to your actual logo file
 
 function Navigation() {
     return (
         <nav>
-            <ul>
-                <li><Link to="/home">Home</Link></li>
-                <li><Link to="/about">Stay</Link></li>
-                <li><Link to="/weekend">Weekend</Link></li>
-                <li><Link to="/activities">Activities</Link></li>
-                <li><Link to="/rsvp">RSVP</Link></li>
-            </ul>
+            <div className="nav-content">
+                <Link to="/home">
+                    <img src={logo} alt="Logo" className="logo" />
+                </Link>
+                <ul>
+                    <li><Link to="/home">Home</Link></li>
+                    <li><Link to="/about">Stay</Link></li>
+                    <li><Link to="/weekend">Weekend</Link></li>
+                    <li><Link to="/activities">Activities</Link></li>
+                    <li><Link to="/rsvp">RSVP</Link></li>
+                </ul>
+            </div>
         </nav>
     );
 }
