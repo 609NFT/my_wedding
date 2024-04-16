f34261ba5a6724072a2b3a1c7466d235bdb36ba8
609NFT <boisjolibrian@gmail.com>
2024-04-16 17:40:55 -0400

    homepage

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <p className="intro_text">
           Hope & Brian
         </p>
         <a
