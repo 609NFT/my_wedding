069918a8aaf412ab28f1f9a5808f188781516a99
609NFT <boisjolibrian@gmail.com>
2024-04-22 19:36:58 -0400

    copy updates

--- src/pages/activities.js
+++ src/pages/activities.js
@@ -6,8 +6,30 @@ function Activities() {
     return (
         <div className='home'>
             <Navigation></Navigation>
-            <p> activities page </p>
+            <div className='content'>
+            <h1> Things To Do </h1>
+            <ul>
+                <li>Stroll and shop Main Street Sag Harbor</li>
+                <li>Shop and stroll Main Street Bridgehmpton</li>
+                <li>Stroll the Long Wharf & Windmill</li>
+                <li>Whaling Museum</li>
+                <li>Morton Refuge (nature walk to the bay)</li>
+            </ul>
+
+            <h2>Beaches</h2>
+
+<ul>
+    <li>Long Beach (bay beach can buy a day pass and park here on the weekends)</li>
+    <li>*Mecox</li>
+    <li>*Sagg Main</li>
+    <li>*Scott Cameron</li>
+</ul>
+
+<p>*Due to town parking permit requirements, you will need to Uber to and from these ocean beaches</p>
         </div>
+
+
+            </div>
     );
 }
 
--- src/pages/weekend.js
+++ src/pages/weekend.js
@@ -6,8 +6,47 @@ function Weekend() {
     return (
         <div className='home'>
             <Navigation></Navigation>
-            <p> weekend page </p>
-        </div>
+            <div className='content'>
+            <h2>Transportation</h2>
+            <h3>To and from the Bridgehampton Surf and Tennis Club</h3>
+
+<p>The Hamptons Hopper is available to take you safely to and from the 
+    venue for the celebration. If you would like to hitch a ride, please 
+    Uber to or park along the road near 15 Northview Hills Court. The Hopper 
+    will leave from 15 Northview Hills Court at 6 pm sharp and return back at 
+    the conclusion of the party departing BSTC at 11:30 pm and returning you back 
+    to 15 Northview Hills Court. One Hopper may be destined for a drop off in Sag 
+    Harbor where the celebration will continue. The other will return to Northview Hills Court.</p>
+
+<p>Please text Donna 516-695-6003 if you’d like a lift!</p>
+
+<h2>Eats</h2>
+<h4>Breakfast</h4>
+<ul>
+    <li>Estia’s Little Kitchen</li>
+    <li>Goldberg’s Bagels</li>
+    <li>Jimmy Jim’s Deli</li>
+    <li>Cromers Market</li>
+    <li>Provisions</li>
+    <li>K Pasa</li>
+</ul>
+
+<h4>Lunch</h4>
+<ul>
+<li>Corner Bar</li>
+<li>Page</li>
+<li>American Hotel</li>
+<li>El Taco Bar</li>
+<li>Estia’s Little Kitchen</li>
+<li>Provisions</li>
+<li>Barrons Cove</li>
+<li>K Pasa</li>
+<li>Sen</li>
+<li>Carissa</li>
+<li>Goldberg’s Bagels</li>
+</ul>
+</div>
+</div>
     );
 }
 
