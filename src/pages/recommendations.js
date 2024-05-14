7f68124f61aa9c94c0f2d1f0209fa96784bf78c4
609NFT <boisjolibrian@gmail.com>
2024-05-13 20:29:22 -0400

    updates

--- src/pages/recommendations.js
+++ src/pages/recommendations.js
@@ -10,62 +10,45 @@ function Activities() {
         {/*<div className='home'>*/}
             <Navigation></Navigation>
             <div className='content'>
-            <h1> Things To Do </h1>
-            <ul>
-                <li>Stroll and shop Main Street Sag Harbor</li>
-                <li>Shop and stroll Main Street Bridgehmpton</li>
-                <li>Stroll the Long Wharf & Windmill</li>
-                <li>Whaling Museum</li>
-                <li>Morton Refuge (nature walk to the bay)</li>
-            </ul>
 
-            <h2>Beaches</h2>
+<h2>Drinks</h2>
+<ul className="two-column-list">
+<li>The Wine Stand at Wolffer Estate<br></br><span><i>Sagaponack</i></span></li>
+<li>Channing Daughters Winery<br></br><span><i>Bridgehampton</i></span></li>
+<li>Sagaponack Farm Distillery<br></br><span><i>Sagaponack</i></span></li>
+<li>Kidd Squid Brewery Co.<br></br><span><i>Sag Harbor</i></span></li>
 
-<ul>
-    <li>Long Beach (bay beach can buy a day pass and park here on the weekends)</li>
-    <li>*Mecox</li>
-    <li>*Sagg Main</li>
-    <li>*Scott Cameron</li>
 </ul>
 
+
+<h2>Eats</h2>
+<ul className="two-column-list">
+<li>Estia’s Little Kitchen<br></br><span><i>Sag Harbor</i></span></li>
+<li>Sag Harbor Tavern<br></br><span><i>Sag Harbor</i></span></li>
+<li>Goldberg’s Bagels<br></br><span><i>Everywhere</i></span></li>
+<li>K Pasa<br></br><span><i>Sag Harbor</i></span></li>
+<li>Baron’s Cove<br></br><span><i>Sag Harbor</i></span></li>
+<li>Sen<br></br><span><i>Sag Harbor</i></span></li>
+<li>Moby’s<br></br><span><i>East Hampton</i></span></li>
+<li>Hampton Chutney<br></br><span><i>East Hampton</i></span></li>
+</ul>
+       
+<h2>Activities</h2>
+<ul className="two-column-list">
+<li>Stroll Main Street and Long Wharf<br></br><span><i>Sag Harbor</i></span></li>
+<li>Morton Refuge (nature walk by the bay)<br></br><span><i>Noyack</i></span></li>
+</ul>
+
+<h2>Beaches</h2>
+<ul className="two-column-list">
+<li>Long Beach (bay beach - day passes available)<br></br><span><i>Sag Harbor</i></span></li>
+<li>Sagg Main Beach*<br></br><span><i>Sagaponack</i></span></li>
+<li>Mecox Beach*<br></br><span><i>Sagaponack</i></span></li>
+<li>Scott Cameron Beach*<br></br><span><i>Sagaponack</i></span></li>
+
+</ul>
 <p>*Due to town parking permit requirements, you will need to Uber to and from these ocean beaches</p>
-       
-<h2>Our Favorites</h2>
-<h4>Breakfast</h4>
-<ul>
-    <li>Estia’s Little Kitchen</li>
-    <li>Goldberg’s Bagels</li>
-    <li>Jimmy Jim’s Deli</li>
-    <li>Cromers Market</li>
-    <li>Provisions</li>
-    <li>K Pasa</li>
-</ul>
 
-<h4>Lunch</h4>
-<ul>
-<li>Corner Bar</li>
-<li>Page</li>
-<li>American Hotel</li>
-<li>El Taco Bar</li>
-<li>Estia’s Little Kitchen</li>
-<li>Provisions</li>
-<li>Barrons Cove</li>
-<li>K Pasa</li>
-<li>Sen</li>
-<li>Carissa</li>
-<li>Goldberg’s Bagels</li>
-</ul>
-
-<h4>Spirits</h4>
-<ul>
-<li>Wollfer Estate Vineyards</li>
-<li>Wolffer Winestand</li>
-<li>Sagaponack Farm Distillery</li>
-<li>Channing Daughters Winery</li>
-<li>Kidd Squid Brewery</li>
-
-</ul>
-       
        
         </div>
 
